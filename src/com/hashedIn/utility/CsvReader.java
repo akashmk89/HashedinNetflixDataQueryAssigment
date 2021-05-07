433ab7e60597912172f6453c287a5ecf3eaca361
Konnur <akkonnur@deloitte.com>
2021-05-07 18:24:15 +0530

    naming conventions

--- src/com/hashedIn/utility/CsvReader.java
+++ src/com/hashedIn/utility/CsvReader.java
@@ -18,23 +18,27 @@ public class CsvReader {
         String dataRow = "";
         BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\netflix_titles.csv"));
         while ((dataRow = bufferedReader.readLine()) != null) {
-            Date date;
             if (count == 0) {
                 count++;
                 continue;
             }
             String[] token = dataRow.split(delimiter);
-            Set<String> director = new HashSet<String>(Arrays.asList(token[3].split(",")));
-            Set<String> cast = new HashSet<String>(Arrays.asList(token[4].replaceAll("\"","").split(",")));
-            Set<String> country = new HashSet<String>(Arrays.asList(token[5].split(",")));
-            Set<String> listedIn = new HashSet<String>(Arrays.asList(token[10].replaceAll("\"","").split(",")));
-            DateFormat format = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
-            date = token[6].isEmpty()? null: format.parse(token[6].replaceAll("\"","").trim());
-            NetflixShow show = new NetflixShow(token[0], token[1], token[2], director, cast, country,date, 5 , token[8], token[9], listedIn, token[11]);
+            Set<String> directors = new HashSet<String>(Arrays.asList(token[3].split(",")));
+            Set<String> casts = new HashSet<String>(Arrays.asList(token[4].replaceAll("\"","").split(",")));
+            Set<String> countries = new HashSet<String>(Arrays.asList(token[5].split(",")));
+            Set<String> genres = new HashSet<String>(Arrays.asList(token[10].replaceAll("\"","").split(",")));
+            NetflixShow show = new NetflixShow(token[0], token[1], token[2], directors, casts, countries,this.getDate(token[6]), 5 , token[8], token[9], genres, token[11]);
             netflixShows.add(show);
         }
         System.out.println("There are total of " +netflixShows.size() + " records in Netflix database");
         return  netflixShows;
     }
+
+    public Date getDate(String token) throws ParseException{
+        Date date = new Date();
+        DateFormat format = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
+        date = token.isEmpty()? null: format.parse(token.replaceAll("\"","").trim());
+        return date;
+       }
     }
 
